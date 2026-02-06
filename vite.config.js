9c3e88b5f2267736481b3396942a4afd692fb1dd
RanjeetRaj444 <ranjeetraj44666@gmail.com>
2026-02-06 23:44:49 +0530

    updated latest package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+        silenceDeprecations: ["import"],
+        logger: {
+          warn: (message, options) => {
+            if (message.includes("legacy-js-api")) return;
+          },
+        },
+      },
+    },
+  },
+});
